eed6a91bf077656055045e3f90f545952ccda657
Ajay <ajaybautista60@gmail.com>
2024-10-28 13:32:11 +0800

    first commit

--- Navbar.jsx
+++ Navbar.jsx
@@ -1,5 +1,5 @@
 import logo from "../assets/jayay.png";
-import { FaLinkedin } from "react-icons/fa";
+import { FaFacebook, FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
@@ -10,10 +10,20 @@ const Navbar = () => {
          <img className="mx-2 w-10" src={logo} alt="logo" />   
     </div>
     <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
+
+
+        <a href="https://github.com/ajaykups">
         <FaGithub />
+        </a>
+
+        <a href="https://www.instagram.com/jaywelryy/">
         <FaInstagram />
-        <FaSquareXTwitter />
+        </a>
+
+        <a href="https://www.facebook.com/meowmeng31">
+        <FaFacebook />
+        </a>
+
     </div>
   </nav>
     
